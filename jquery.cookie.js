84f130b31792922cd495db42e13a88da6340026c
caichicong <franzcai80@gmail.com>
2013-08-05 15:04:50 +0800

    修改了插件的加载方式

--- jquery.cookie.js
+++ jquery.cookie.js
@@ -5,15 +5,7 @@
  * Copyright 2013 Klaus Hartl
  * Released under the MIT license
  */
-(function (factory) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD. Register as anonymous module.
-		define(['jquery'], factory);
-	} else {
-		// Browser globals.
-		factory(jQuery);
-	}
-}(function ($) {
+ (function($) {
 
 	var pluses = /\+/g;
 
@@ -28,41 +20,37 @@
 	function converted(s) {
 		if (s.indexOf('"') === 0) {
 			// This is a quoted cookie as according to RFC2068, unescape
-			s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
+			s = s.slice(1, - 1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
 		}
 		try {
 			return config.json ? JSON.parse(s) : s;
 		} catch(er) {}
 	}
 
-	var config = $.cookie = function (key, value, options) {
+	var config = $.cookie = function(key, value, options) {
 
 		// write
 		if (value !== undefined) {
-			options = $.extend({}, config.defaults, options);
+			options = $.extend({},
+			config.defaults, options);
 
 			if (typeof options.expires === 'number') {
-				var days = options.expires, t = options.expires = new Date();
+				var days = options.expires,
+				t = options.expires = new Date();
 				t.setDate(t.getDate() + days);
 			}
 
 			value = config.json ? JSON.stringify(value) : String(value);
 
 			return (document.cookie = [
-				config.raw ? key : encodeURIComponent(key),
-				'=',
-				config.raw ? value : encodeURIComponent(value),
-				options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
-				options.path    ? '; path=' + options.path : '',
-				options.domain  ? '; domain=' + options.domain : '',
-				options.secure  ? '; secure' : ''
-			].join(''));
+			config.raw ? key: encodeURIComponent(key), '=', config.raw ? value: encodeURIComponent(value), options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+			options.path ? '; path=' + options.path: '', options.domain ? '; domain=' + options.domain: '', options.secure ? '; secure': ''].join(''));
 		}
 
 		// read
-		var decode = config.raw ? raw : decoded;
+		var decode = config.raw ? raw: decoded;
 		var cookies = document.cookie.split('; ');
-		var result = key ? undefined : {};
+		var result = key ? undefined: {};
 		for (var i = 0, l = cookies.length; i < l; i++) {
 			var parts = cookies[i].split('=');
 			var name = decode(parts.shift());
@@ -83,13 +71,17 @@
 
 	config.defaults = {};
 
-	$.removeCookie = function (key, options) {
+	$.removeCookie = function(key, options) {
 		if ($.cookie(key) !== undefined) {
 			// Must not alter options, thus extending a fresh object...
-			$.cookie(key, '', $.extend({}, options, { expires: -1 }));
+			$.cookie(key, '', $.extend({},
+			options, {
+				expires: - 1
+			}));
 			return true;
 		}
 		return false;
 	};
 
-}));
+})(jQuery);
+
